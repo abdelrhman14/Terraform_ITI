fa6cb22cff125cd93eb06b4513eaec77d82e8d1f
Abdelrahman Tarek Mahmoud <42601017+abdelrhman14@users.noreply.github.com>
2022-09-25 00:04:38 +0200

    Add files via upload

---
+++ dev.tfvars
@@ -0,0 +1,12 @@
+vpc_cidr = "172.1.0.0/16"
+
+subnet_public1_cidr = "10.0.1.0/24"
+subnet_public2_cidr = "10.1.0.0/24"
+subnet_private1_cidr = "10.0.2.0/24"
+subnet_private2_cidr = "10.0.3.0/24"
+
+route_table_public_cidr = "0.0.0.0/0"
+route_table_private1_cidr = "0.0.0.0/0"
+route_table_private2_cidr = "0.0.0.0/0"
+
+region         = "eu-central-1"
---
+++ main.tf
@@ -0,0 +1,15 @@
+module "iti_network" {
+  source = "/home/abdelrahman-tarek/Documents/AWS/network"
+  vpc_cidr = var.vpc_cidr
+
+  subnet_public1_cidr = var.subnet_public1_cidr
+  subnet_public2_cidr = var.subnet_public2_cidr
+  subnet_private1_cidr = var.subnet_private1_cidr
+  subnet_private2_cidr = var.subnet_private2_cidr
+
+  route_table_public_cidr = var.route_table_public_cidr
+  route_table_private1_cidr = var.route_table_private1_cidr
+  route_table_private2_cidr = var.route_table_private2_cidr
+
+
+}
